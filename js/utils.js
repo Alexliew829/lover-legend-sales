abffd57e559055da6b58218d4243e8b5d2258877
Alexliew829 <alexliew2026@gmail.com>
2026-08-10 22:51:03 +0800

    Add files via upload

--- js/utils.js
+++ js/utils.js
@@ -300,7 +300,7 @@ function monthAfter(m){
 }
 
 
-// V15.3: Fair 地点键值忽略所有空格及英文字母大小写，但保留符号。
+// V15.4: Fair 地点键值忽略所有空格及英文字母大小写，但保留符号。
 // 例如 "JB AEON Mall , Terbau City" 与 "JB AEON Mall,Terbau City" 是同一地点。
 function normalizeFairLocationKey(value){
   return canonicalLocation(value||"")
