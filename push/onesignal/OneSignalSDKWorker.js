4060f1a62278b87b372ef56c9334590f65bdf0d5
Alexliew829 <alexliew2026@gmail.com>
2026-08-17 01:49:07 +0800

    Add files via upload

--- push/onesignal/OneSignalSDKWorker.js
+++ push/onesignal/OneSignalSDKWorker.js
@@ -1,1 +1,1 @@
-// V19.7 - keep this URL stable for existing OneSignal subscriptions.\nimportScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");\n
+// V19.8 - keep this URL stable for existing OneSignal subscriptions.\nimportScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");\n
