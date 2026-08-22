596867bca7abd01681be2513f840d641d6453e90
Alexliew829 <alexliew2026@gmail.com>
2026-08-22 15:58:59 +0800

    Add files via upload

--- push/onesignal/OneSignalSDKUpdaterWorker.js
+++ push/onesignal/OneSignalSDKUpdaterWorker.js
@@ -1,2 +1,2 @@
-// V25.8 - OneSignal updater worker.
+// V25.9 - OneSignal updater worker.
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDKUpdaterWorker.js");
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// Lover Legend Sales PWA Service Worker V25.8
+// Lover Legend Sales PWA Service Worker V25.9
 // OneSignal uses its own worker:
 // /push/onesignal/OneSignalSDKWorker.js
 
