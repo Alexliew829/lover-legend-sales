f0829565d3089897c63ce970cd1508b4e8ba8c54
Alexliew829 <alexliew2026@gmail.com>
2026-08-26 04:16:12 +0800

    Add files via upload

--- OneSignalSDKUpdaterWorker.js
+++ OneSignalSDKUpdaterWorker.js
@@ -1,2 +1,2 @@
-// V29.0 - Legacy OneSignal updater worker path retained.
+// V29.1 - Legacy OneSignal updater worker path retained.
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDKUpdaterWorker.js");
--- push/onesignal/OneSignalSDKWorker.js
+++ push/onesignal/OneSignalSDKWorker.js
@@ -1,2 +1,2 @@
-// V29.0 - OneSignal Web Push worker. Keep this path stable.
+// V29.1 - OneSignal Web Push worker. Keep this path stable.
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
