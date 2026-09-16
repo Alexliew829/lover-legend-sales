906aa2bbc39da029a0f1ab95416bcf50b1f4dc7c
Alexliew829 <alexliew2026@gmail.com>
2026-09-16 19:15:43 +0800

    Delete Frontend directory

--- Frontend/OneSignalSDKUpdaterWorker.js
+++
@@ -1,2 +0,0 @@
-// V32.5 - Legacy OneSignal updater worker path retained.
-importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDKUpdaterWorker.js");
--- Frontend/push/onesignal/OneSignalSDKUpdaterWorker.js
+++
@@ -1,2 +0,0 @@
-// V32.5 - OneSignal updater worker.
-importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDKUpdaterWorker.js");
