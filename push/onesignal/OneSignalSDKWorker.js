b0d1d51c6cbbba10efc1e9758b271bafdf5c34d4
Alexliew829 <alexliew2026@gmail.com>
2026-09-22 17:53:08 +0800

    Add files via upload

---
+++ push/onesignal/OneSignalSDKWorker.js
@@ -0,0 +1,2 @@
+// V32.5 - OneSignal Web Push worker. Keep this path stable.
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
---
+++ sw.js
@@ -0,0 +1,15 @@
+// Lover Legend Sales PWA Service Worker V52.0
+// OneSignal uses its own worker:
+// /push/onesignal/OneSignalSDKWorker.js
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil((async()=>{
+    const names=await caches.keys();
+    await Promise.all(names.map(name=>caches.delete(name)));
+    await self.clients.claim();
+  })());
+});
